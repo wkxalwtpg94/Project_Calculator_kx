66442a5e2bb9f19a053f9535df33b2760d463363
Ken <kenxhen@gmail.com>
2024-06-30 16:57:04 +0800

    Only allow evaluation of single pair of numbers at a time

--- myscript.js
+++ myscript.js
@@ -1,18 +1,22 @@
 // Create Basic Math Operators
 function add (a, b) {
-    return Number(a) + Number(b)
+    return Math.round((Number(a) + Number(b))*1000)/1000
 };
 
 function subtract (a, b) {
-    return a - b
+    return Math.round((a - b)*1000)/1000
 };
 
 function multiply (a, b) {
-    return a * b
+    return Math.round((a * b)*1000)/1000
 };
 
 function divide (a, b) {
-    return a / b
+    if (b == 0) {
+        return "Error: Cannot divide zero"
+    } else {
+    return Math.round((a / b)*1000)/1000
+    }
 }
 
 // Declare Variables
@@ -42,10 +46,12 @@ function operate (operator, firstNumber, secondNumber) {
 let display = document.querySelector(".display");
 let storedDisplay = display.textContent;
 let splitDisplay;
+let pattern;
+let answer;
 
-// Buttons to Populate Display
-let buttonsToDisplay = document.querySelectorAll(".digit_button, .operator_button")
-buttonsToDisplay.forEach((button) => {
+// Digit Buttons to populate display
+let digitToDisplay = document.querySelectorAll(".digit_button")
+digitToDisplay.forEach((button) => {
     button.addEventListener("click", () => {
         if (display.textContent == "0") {
             display.textContent = ""
@@ -53,11 +59,10 @@ buttonsToDisplay.forEach((button) => {
         display.textContent = display.textContent + button.value
         storedDisplay = display.textContent
 
-
-
+        
     // Make the calculator work behind the scenes
     // Store first and second value
-    let pattern = /([+\-x÷])/;
+    pattern = /([+\-x÷])/;
     
     splitDisplay = (storedDisplay.replace(/\s/g, "")).split(pattern);
     console.log(splitDisplay)
@@ -65,14 +70,39 @@ buttonsToDisplay.forEach((button) => {
 
 });
 
+// Operator Buttons to populate display
+let operatorToDisplay = document.querySelectorAll(".operator_button")
+operatorToDisplay.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (display.textContent == "0") {
+            display.textContent = ""
+        }
+
+        if (splitDisplay[0] != null && splitDisplay[1] != null && splitDisplay[2] != null) {
+            answer  = operate(splitDisplay[1], splitDisplay[0], splitDisplay[2])
+            display.textContent = answer + button.value
+        } else {
+        display.textContent = display.textContent + button.value
+        storedDisplay = display.textContent
+        }
+
+        
+    // Make the calculator work behind the scenes
+    // Store first and second value
+    pattern = /([+\-x÷])/;
     
+    splitDisplay = (storedDisplay.replace(/\s/g, "")).split(pattern);
+    console.log(splitDisplay)
+    });
+
+});
 
 
 
 // Create function behind equals button
 let equalsButton = document.querySelector(".equals_button");
 equalsButton.addEventListener("click", () => {
-    let answer  = operate(splitDisplay[1], splitDisplay[0], splitDisplay[2])
+    answer  = operate(splitDisplay[1], splitDisplay[0], splitDisplay[2])
     console.log(answer)
     display.textContent = answer
 });
